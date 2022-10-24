60c9aba9c52f2724e2490b86aeddaa8320654532
petrero <petrero@gmail.com>
2022-10-24 21:45:08 +0300

    9.6 Idea model and migrations - Tailor idea factory file and run php artisan migrate:fresh --seed to populate our database with dummy data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,22 +3,16 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Idea;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+    public function run() {
+        Idea::factory(30)->create();
     }
 }
