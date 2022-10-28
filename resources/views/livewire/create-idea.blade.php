d9ed5530cba4dcf88a20b21779797a45b6fceabd
petrero <petrero@gmail.com>
2022-10-28 19:02:31 +0300

    15.5 Make the confirmation that an ideas was created successfully to dissapear after 6 sec using alpinejs

--- resources/views/livewire/create-idea.blade.php
+++ resources/views/livewire/create-idea.blade.php
@@ -53,7 +53,16 @@
 
         <div>
             @if('success_message')
-                <div class="text-green mt-4">
+                <div
+                    x-data="{ isVisible: true }"
+                    x-init="
+                        setTimeout(() => {
+                            isVisible = false
+                        }, 5000)
+                    "
+                    x-show.transition.duration.1000ms="isVisible"
+                    class="text-green mt-4"
+                >
                     {{ session('success_message') }}
                 </div>
             @endif
