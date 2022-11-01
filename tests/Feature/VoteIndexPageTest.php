59f5b772584bbb314a95bf487b3a037bf418988e
petrero <petrero@gmail.com>
2022-11-01 13:19:38 +0200

    23.4 Correct failing tests

--- tests/Feature/VoteIndexPageTest.php
+++ tests/Feature/VoteIndexPageTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Http\Livewire\IdeaIndex;
 use App\Http\Livewire\IdeaShow;
+use App\Http\Livewire\IdeasIndex;
 use App\Models\Category;
 use App\Models\Idea;
 use App\Models\Status;
@@ -38,7 +39,7 @@ class VoteIndexPageTest extends TestCase {
     }
 
     /** @test */
-    public function index_page_correctly_receives_votes_count(){
+    public function ideas_index_livewire_component_correctly_receives_votes_count(){
         $user = User::factory()->create();
         $userB = User::factory()->create();
 
@@ -63,7 +64,7 @@ class VoteIndexPageTest extends TestCase {
             'user_id' => $userB->id
         ]);
 
-        $this->get(route('idea.index'))
+        Livewire::test(IdeasIndex::class)
             ->assertViewHas('ideas', function($ideas){
                 return $ideas->first()->votes_count == 2;
             });
