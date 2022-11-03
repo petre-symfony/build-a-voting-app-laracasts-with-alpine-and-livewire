6cf025429c96697c00450510680e9351669db63e
petrero <petrero@gmail.com>
2022-11-03 09:47:12 +0200

    24.5 In show page we start without a query string and preestablished status whereas in main page we show all ideas at start

--- app/Http/Livewire/StatusFilters.php
+++ app/Http/Livewire/StatusFilters.php
@@ -4,8 +4,17 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 
+use Illuminate\Support\Facades\Route;
+
 class StatusFilters extends Component {
     public $status = 'All';
+
+    public function mount() {
+        if (Route::currentRouteName() === 'idea.show') {
+            $this->status = null;
+            $this->queryString = [];
+        }
+    }
 
     public function setStatus($newStatus){
         $this->status = $newStatus;
